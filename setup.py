41e2665ae1e08581da1f307eadad2d1ad345512e
hash3liZer <sheikhshameerkashif@protonmail.com>
2020-08-26 17:28:02 +0530

    Version 3.2 Stable

--- setup.py
+++ setup.py
@@ -9,26 +9,25 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="Subrake",
-    version="3.1",
+    name="subrake",
+    version="3.2",
     description="A Subdomain Enumeration and Validation tool for Bug Bounty and Pentesters.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/hash3liZer/Subrake",
     author="hash3liZer",
-    author_email="admin@shellvoide.com",
+    author_email="sheikhshameerkashif@protonmail.com",
     license="GPLv3",
-    download_url = 'https://github.com/hash3liZer/Subrake/archive/v3.1.tar.gz',
+    download_url = 'https://github.com/hash3liZer/Subrake/archive/v3.2.tar.gz',
     keywords=['bugbounty', 'bugbountytips', 'tool', 'subdomain', 'pentesting'],
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
         "Operating System :: POSIX",
     ],
-    packages=["subrake"],
+    packages=["subrake", "subrake.handlers"],
     include_package_data=True,
     install_requires=["requests", "dnspython", "beautifulsoup4"],
     entry_points={
