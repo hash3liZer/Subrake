a781e56a8cdb36821113a2177bd3a18f011bdc2c
Shameer Kashif <sheikhshameerkashif@protonmail.com>
2023-05-16 00:26:18 +0700

    Updatde bash runners for more solid input

--- utils/bashrunner.sh
+++ utils/bashrunner.sh
@@ -2,60 +2,72 @@
 
 trap '' INT
 
-echo -n "Enter Domain name: "
-read domain
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+RESET="\e[0m"
 
-echo -n "Do you want to run online module [Y/n]: "
-read omodule
+while true; do
+    echo -en "${RED}[?]${RESET} Enter Domain name                                 : "
+    read domain
 
-echo -n "Do you want to subcast [Y/n]: "
-read subcast
+    echo -en "${RED}[?]${RESET}Do you want to run online module [Y/n]             : "
+    read omodule
 
-echo -n "Want to provide any wordlist [default/empty for no]: "
-read wordlist
+    echo -en "${RED}[?]${RESET}Do you want to subcast [Y/n]                       : "
+    read subcast
 
-echo -n "Any IPs you want to exclude [comma-separated]: "
-read excludelist
+    echo -en "${RED}[?]${RESET}Want to provide any wordlist [default/empty for no]: "
+    read wordlist
 
-echo -n "Specify Ports you want to scan [Empty to leave]: "
-read ports
+    echo -en "${RED}[?]${RESET}Any IPs you want to exclude [comma-separated]      : "
+    read excludelist
 
-echo -n "Number of threads to generate [25]: "
-read threads
+    echo -en "${RED}[?]${RESET}Specify Ports you want to scan [Empty to leave]    : "
+    read ports
 
-args="-d $domain -o /opt/subtakes/$domain/subdomains.txt --csv /opt/subtakes/$domain/report.csv --filter"
+    echo -en "${RED}[?]${RESET}Number of threads to generate [25]                 : "
+    read threads
 
-if [ "$omodule" != "Y" ] && [ "$omodule" != "y" ]; then
-    args="$args --skip-search"
-fi
+    args="-d $domain -o /opt/subtakes/$domain/subdomains.txt --csv /opt/subtakes/$domain/report.csv --filter"
 
-if [ "$subcast" != "Y" ] && [ "$subcast" != "y" ]; then
-    args="$args --skip-subcast"
-else
-    args="$args --only-sublister"
-fi
-
-if [ "$wordlist" != "" ]; then
-    if [ "$wordlist" == "default" ]; then
-    args="$args --wordlists /opt/SecLists/Discovery/DNS/subdomains-top1million-5000.txt"
-    else
-    args="$args --wordlists $wordlist"
+    if [ "$omodule" != "Y" ] && [ "$omodule" != "y" ]; then
+        args="$args --skip-search"
     fi
-fi
 
-if [ "$excludelist" != "" ]; then
-    args="$args --exclude-ips '$excludelist'"
-fi
+    if [ "$subcast" != "Y" ] && [ "$subcast" != "y" ]; then
+        args="$args --skip-subcast"
+    else
+        args="$args --only-sublister"
+    fi
 
-if [ "$ports" != "" ]; then
-    args="$args --ports '$ports'"
-fi
+    if [ "$wordlist" != "" ]; then
+        if [ "$wordlist" == "default" ]; then
+        args="$args --wordlists /opt/SecLists/Discovery/DNS/subdomains-top1million-5000.txt"
+        else
+        args="$args --wordlists $wordlist"
+        fi
+    fi
 
-if [ "$threads" != "" ]; then
-    args="$args --threads $threads"
-fi
+    if [ "$excludelist" != "" ]; then
+        args="$args --exclude-ips '$excludelist'"
+    fi
 
-mkdir /opt/subtakes/$domain
+    if [ "$ports" != "" ]; then
+        args="$args --ports '$ports'"
+    fi
 
-subrake $args
-exit
+    if [ "$threads" != "" ]; then
+        args="$args --threads $threads"
+    fi
+
+    mkdir -p /opt/subtakes/$domain
+
+    screen -S $domain -dm bash -c "subtakes $args; echo;; echo; echo "${MAGENTA}[-]${RESET} Press any key to continue..."; read; exit"
+    screen -r $domain
+    clear
+done
