39bc150a4b803ff22cd0cf4dd85f5ea70c27ae99
Shameer Kashif <sheikhshameerkashif@protonmail.com>
2023-05-17 04:03:12 +0700

    Updated get_tables script

--- utils/get_tables.sh
+++ utils/get_tables.sh
@@ -6,13 +6,15 @@ folder="/usr/share/cockpit/static/subtakes"
 directories=$(find "$folder" -mindepth 1 -maxdepth 1 -type d)
 
 declare -A rtval
-declare -A section
 
 # Loop through each directory
 for dir in $directories; do
-    # get base name of dir
+    # Get base name of dir
     basename=$(basename "$dir")
     sessionname="${basename//.}"
+
+    # Initialize the section dictionary
+    declare -A section
 
     # Emptying the section
     section["name"]="$basename"
@@ -23,16 +25,16 @@ for dir in $directories; do
 
     # Date Collection
     if [ -e "$dir/datetime.txt" ]; then
-        section["datetime"]=$(cat "$dir/datetime.txt")
+        section["datetime"]="$(cat "$dir/datetime.txt")"
     fi
 
     # Subdomain Collection
     if [ -e "$dir/report.csv" ]; then
-        section["subdomains"]=$(tail -n +2 "$dir/report.csv" | grep -i "$basename" | wc -l)
+        section["subdomains"]="$(tail -n +2 "$dir/report.csv" | grep -i "$basename" | wc -l)"
     fi
 
     # Status Collection
-    if [ tmux has-session -t "$sessionname" 2>/dev/null ]; then
+    if tmux has-session -t "$sessionname" 2>/dev/null; then
         section["status"]="Running"
     else
         if [ -e "$dir/report.csv" ] && [ -e "$dir/subdomains.txt" ] && [ -e "$dir/datetime.txt" ]; then
@@ -42,15 +44,15 @@ for dir in $directories; do
         fi
     fi
 
-    # Takover Collection
+    # Takeover Collection
     if [ -e "$dir/report.csv" ] && [ -e "$dir/subdomains.txt" ] && [ -e "$dir/datetime.txt" ]; then
-        section["takeovers"]=$(tail -n +2 "$dir/report.csv" | grep -i ",Possible" | wc -l)
+        section["takeovers"]="$(tail -n +2 "$dir/report.csv" | grep -i ",Possible" | wc -l)"
     fi
 
-    # Add the section to the array
+    # Add the section to the rtval dictionary
     rtval["$basename"]="${section[@]}"
-
-    # Convert the associative array to a JSON string
-    json_string=$(declare -p json | awk -F= '{print "\"" $1 "\":" $2}' | tr -d ' ' | tr '\n' ',' | sed 's/,$//')
-    echo "$json_string"
 done
+
+# Convert the rtval dictionary to JSON using jq
+json_string=$(declare -p rtval | jq -c .)
+echo "$json_string"
